d49a878228c757aa7309f06388d5e682c38a406a
ai4java <eyal@ai4java.com>
2019-12-03 23:40:36 -0500

    Chapter 12 files

---
+++ Chapter12/01-gp-even-parity.py
@@ -0,0 +1,147 @@
+import random
+import operator
+
+import numpy as np
+
+from deap import base
+from deap import creator
+from deap import tools
+from deap import gp
+
+import itertools
+import matplotlib.pyplot as plt
+import networkx as nx
+
+import elitism
+
+# problem constants:
+NUM_INPUTS = 3
+NUM_COMBINATIONS = 2 ** NUM_INPUTS
+
+# Genetic Algorithm constants:
+POPULATION_SIZE = 60
+P_CROSSOVER = 0.9
+P_MUTATION = 0.5
+MAX_GENERATIONS = 20
+HALL_OF_FAME_SIZE = 10
+
+# Genetic Programming specific constans:
+MIN_TREE_HEIGHT = 3
+MAX_TREE_HEIGHT = 5
+LIMIT_TREE_HEIGHT = 17
+MUT_MIN_TREE_HEIGHT = 0
+MUT_MAX_TREE_HEIGHT = 2
+
+
+# set the random seed:
+RANDOM_SEED = 42
+random.seed(RANDOM_SEED)
+
+toolbox = base.Toolbox()
+
+# calculate the truth table of even parity check:
+parityIn = list(itertools.product([0, 1], repeat=NUM_INPUTS))
+parityOut = []
+for row in parityIn:
+    parityOut.append(sum(row) % 2)
+
+# create the primitive set:
+pset = gp.PrimitiveSet("main", NUM_INPUTS, "in_")
+pset.addPrimitive(operator.and_, 2)
+pset.addPrimitive(operator.or_, 2)
+pset.addPrimitive(operator.xor, 2)
+pset.addPrimitive(operator.not_, 1)
+
+# add terminal values:
+pset.addTerminal(1)
+pset.addTerminal(0)
+
+# define a single objective, minimizing fitness strategy:
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+
+# create the Individual class based on the primitive tree:
+creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
+
+# create a helper function for creating random trees using the primitive set:
+toolbox.register("expr", gp.genFull, pset=pset, min_=MIN_TREE_HEIGHT, max_=MAX_TREE_HEIGHT)
+
+# create the individual operator to fill up an Individual instance:
+toolbox.register("individualCreator", tools.initIterate, creator.Individual, toolbox.expr)
+
+# create the population operator to generate a list of individuals:
+toolbox.register("populationCreator", tools.initRepeat, list, toolbox.individualCreator)
+
+# create an operator to compile the primitive tree into python code:
+toolbox.register("compile", gp.compile, pset=pset)
+
+# calculate the difference between the results of the
+# generated function and the expected parity results:
+def parityError(individual):
+    func = toolbox.compile(expr=individual)
+    return sum(func(*pIn) != pOut for pIn, pOut in zip(parityIn, parityOut))
+
+# fitness measure:
+def getCost(individual):
+    return parityError(individual), # return a tuple
+
+toolbox.register("evaluate", getCost)
+
+# genetic operators:
+toolbox.register("select", tools.selTournament, tournsize=2)
+toolbox.register("mate", gp.cxOnePoint)
+toolbox.register("expr_mut", gp.genGrow, min_=MUT_MIN_TREE_HEIGHT, max_=MUT_MAX_TREE_HEIGHT)
+toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+
+# bloat control:
+toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=LIMIT_TREE_HEIGHT))
+toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=LIMIT_TREE_HEIGHT))
+
+# Genetic Algorithm flow:
+def main():
+    # create initial population (generation 0):
+    population = toolbox.populationCreator(n=POPULATION_SIZE)
+
+    # prepare the statistics object:
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("min", np.min)
+    stats.register("avg", np.mean)
+
+    # define the hall-of-fame object:
+    hof = tools.HallOfFame(HALL_OF_FAME_SIZE)
+
+    # perform the Genetic Algorithm flow with elitism:
+    population, logbook = elitism.eaSimpleWithElitism(population,
+                                                      toolbox,
+                                                      cxpb=P_CROSSOVER,
+                                                      mutpb=P_MUTATION,
+                                                      ngen=MAX_GENERATIONS,
+                                                      stats=stats,
+                                                      halloffame=hof,
+                                                      verbose=True)
+
+    # print info for best solution found:
+    best = hof.items[0]
+    print("-- Best Individual = ", best)
+    print("-- length={}, height={}".format(len(best), best.height))
+    print("-- Best Fitness = ", best.fitness.values[0])
+
+
+    # plot best tree:
+    nodes, edges, labels = gp.graph(best)
+    g = nx.Graph()
+    g.add_nodes_from(nodes)
+    g.add_edges_from(edges)
+    pos = nx.spring_layout(g)
+
+    nx.draw_networkx_nodes(g, pos, node_color='cyan')
+    nx.draw_networkx_nodes(g, pos, nodelist=[0], node_color='red', node_size=400)
+
+    nx.draw_networkx_edges(g, pos)
+    nx.draw_networkx_labels(g, pos, **{"labels": labels, "font_size": 8})
+
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ Chapter12/03-pso-himmelblau.py
@@ -0,0 +1,131 @@
+import operator
+import random
+
+import math
+import numpy as np
+
+from deap import base
+from deap import creator
+from deap import tools
+
+# constants:
+DIMENSIONS = 2
+POPULATION_SIZE = 20
+MAX_GENERATIONS = 500
+MIN_START_POSITION, MAX_START_POSITION = -5, 5
+MIN_SPEED, MAX_SPEED = -3, 3
+MAX_LOCAL_UPDATE_FACTOR = MAX_GLOBAL_UPDATE_FACTOR = 2.0
+
+# set the random seed:
+RANDOM_SEED = 42
+np.random.seed(RANDOM_SEED)
+
+toolbox = base.Toolbox()
+
+# define a single objective, minimizing fitness strategy:
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+
+# define the particle class based on ndarray:
+creator.create("Particle", np.ndarray, fitness=creator.FitnessMin, speed=None, best=None)
+
+# create and initialize a new particle:
+def createParticle():
+    particle = creator.Particle(np.random.uniform(MIN_START_POSITION,
+                                                  MAX_START_POSITION,
+                                                  DIMENSIONS))
+    particle.speed = np.random.uniform(MIN_SPEED, MAX_SPEED, DIMENSIONS)
+    return particle
+
+# create the 'particleCreator' operator to fill up a particle instance:
+toolbox.register("particleCreator", createParticle)
+
+
+# create the 'population' operator to generate a list of particles:
+toolbox.register("populationCreator", tools.initRepeat, list, toolbox.particleCreator)
+
+
+def updateParticle(particle, best):
+
+    # create random factors:
+    localUpdateFactor = np.random.uniform(0, MAX_LOCAL_UPDATE_FACTOR, particle.size)
+    globalUpdateFactor = np.random.uniform(0, MAX_GLOBAL_UPDATE_FACTOR, particle.size)
+
+    # calculate local and global speed updates:
+    localSpeedUpdate = localUpdateFactor * (particle.best - particle)
+    globalSpeedUpdate = globalUpdateFactor * (best - particle)
+
+    # scalculate updated speed:
+    particle.speed = particle.speed + (localSpeedUpdate + globalSpeedUpdate)
+
+    # enforce limits on the updated speed:
+    particle.speed = np.clip(particle.speed, MIN_SPEED, MAX_SPEED)
+
+    # replace particle position with old-position + speed:
+    particle[:] = particle + particle.speed
+
+
+toolbox.register("update", updateParticle)
+
+
+# Himmelblau function:
+def himmelblau(particle):
+    x = particle[0]
+    y = particle[1]
+    f = (x ** 2 + y - 11) ** 2 + (x + y ** 2 - 7) ** 2
+    return f,  # return a tuple
+
+
+toolbox.register("evaluate", himmelblau)
+
+
+def main():
+    # create the population of particle population:
+    population = toolbox.populationCreator(n=POPULATION_SIZE)
+
+    # prepare the statistics object:
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("min", np.min)
+    stats.register("avg", np.mean)
+
+    logbook = tools.Logbook()
+    logbook.header = ["gen", "evals"] + stats.fields
+
+    best = None
+
+    for generation in range(MAX_GENERATIONS):
+
+        # evaluate all particles in polulation:
+        for particle in population:
+
+            # find the fitness of the particle:
+            particle.fitness.values = toolbox.evaluate(particle)
+
+            # particle best needs to be updated:
+            if particle.best is None or particle.best.size == 0 or particle.best.fitness < particle.fitness:
+                particle.best = creator.Particle(particle)
+                particle.best.fitness.values = particle.fitness.values
+
+            # global best needs to be updated:
+            if best is None or best.size == 0 or best.fitness < particle.fitness:
+                best = creator.Particle(particle)
+                best.fitness.values = particle.fitness.values
+
+        # update each particle's speed and position:
+        for particle in population:
+            toolbox.update(particle, best)
+
+        # record the statistics for the current generation and print it:
+        logbook.record(gen=generation, evals=len(population), **stats.compile(population))
+        print(logbook.stream)
+
+    # print info for best solution found:
+    print("-- Best Particle = ", best)
+    print("-- Best Fitness = ", best.fitness.values[0])
+
+    import matplotlib.pyplot as plt
+    # extract statistics:
+    minFitnessValues, meanFitnessValues = logbook.select("min", "avg")
+
+
+if __name__ == "__main__":
+    main()
